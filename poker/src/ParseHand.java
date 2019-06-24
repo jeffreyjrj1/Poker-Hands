bbfe72f472c37b244f026c6c7bfc2a37e3eb6827
jeff <jeffrey.jrj1010@gmail.com>
2019-06-23 23:52:03 -0400

    A class originally made for manual creation of hands, ended up acting as a method for testing custom hands to see if comparisons worked properly.

--- poker/src/ParseHand.java
+++ poker/src/ParseHand.java
@@ -1,39 +1,37 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class ParseHand {
+public class ParseHand implements ValidCards{
 
     private String newHand;
-    private final String ranks[] = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
-    private final String suits[] = {"C", "D", "H", "S"};
     private ArrayList<Card> myCards = new ArrayList();
 
     public Hand addCards(String preParsedHand) {
         this.newHand = preParsedHand;
 
         for (int i = 0; i < newHand.length(); ++i) {
-            if (this.newHand == " ") {
+            if (this.newHand.charAt(i) == ' ') {
                 continue;
             }
-            if (!validateRank(newHand.charAt(i))) {
+            if (!validateRank(this.newHand.charAt(i))) {
                 throw new RuntimeException("invalid rank" + newHand.charAt(i));
             }
             if (!validateSuit(newHand.charAt(i + 1))) {
                 throw new RuntimeException("invalid suit" + newHand.charAt(i + 1));
             }
             else {
-                myCards.add(new Card(newHand.charAt(i), newHand.charAt(i + 1)));
+                this.myCards.add(new Card(newHand.charAt(i), newHand.charAt(i + 1)));
                 ++i;
             }
         }
 
-        return new Hand(myCards);
+        return new Hand(this.myCards);
 
     }
 
     public boolean validateRank(char rank) {
         for (int i = 0; i < ranks.length; ++i) {
-            if (rank == ranks[i].charAt(0)) {
+            if (rank == ranks[i]) {
                 return true;
             }
         }
@@ -42,7 +40,7 @@ public class ParseHand {
 
     public boolean validateSuit(char suit) {
         for (int i = 0; i < suits.length; ++i) {
-            if (suit == suits[i].charAt(0)) {
+            if (suit == suits[i]) {
                 return true;
             }
         }
